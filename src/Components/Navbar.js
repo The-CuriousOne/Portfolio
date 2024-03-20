b3a22e09d160c7cc56fc2d85841ed64a515fc429
Abhigyan Kashyap <abhigyankashyap04@gmail.com>
2024-03-21 04:57:13 +0530

    NAVBAR RESPONSIVE

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,38 @@
 import React, { useState, useEffect } from "react";
 import "./Navbar.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Navbar = () => {
+
+  const [menuTab, setMenuTab] = useState(false);
+  const [customMenuIcon, setCustomMenuIcon] = useState(true);
+  
+
+  const [visible, setVisible] = useState(true);
+  const [scrollPos, setscrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScrolls = () =>{
+      const currentScrollPos = window.scrollY;
+      const visible = scrollPos > currentScrollPos;
+      setscrollPos(currentScrollPos);
+      setVisible(visible);
+    };
+
+    window.addEventListener('scroll',handleScrolls);
+    return () => {
+      window.removeEventListener('scroll',handleScrolls);
+    }
+  },[scrollPos]);
+
+  const menuTabStyle = {
+    transform: visible && menuTab?'translateX(0)':'translateX(100%)',
+    transition: 'transform 0.5s'
+  };
+
+
+
   const redirectToPage = () => {
     window.location.href = "/contact_me";
   };
@@ -60,6 +91,45 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+      </div>
+
+      <div className="n-right-small">
+        <div className={`customMenuIcon ${customMenuIcon ? '' : 'customMenuIconClose'}`}>
+          <MenuIcon fontSize="large" onClick={() => {setCustomMenuIcon(false); setMenuTab(true);}}/>
+        </div>
+      </div>
+      <div className={`menuTab ${menuTab ? 'menuTabOpen' : ''}`} style={menuTabStyle}> 
+        <ul>
+          <li><CloseIcon className='customCloseIcon' fontSize="large" onClick={() => {setCustomMenuIcon(true); setMenuTab(false)}}/></li>
+          <li className={currentPath === "/" ? "active" : ""}>
+              <a href="/">
+                <h3>Home</h3>
+              </a>
+            </li>
+            <li className={currentPath === "/domain" ? "active" : ""}>
+              <a href="/domain">
+                <h3>Domains</h3>
+              </a>
+            </li>
+            <li className={currentPath === "/experience" ? "active" : ""}>
+              <a href="/experience">
+                <h3>Experience</h3>
+              </a>
+            </li>
+            <li className={currentPath === "/about_me" ? "active" : ""}>
+              <a href="/about_me">
+                <h3>About</h3>
+              </a>
+            </li>
+            <li>
+              <button className="button" onClick={redirectToPage}>
+                Contact Me
+                <div className="hoverEffect">
+                  <div></div>
+                </div>
+              </button>
+            </li>
+        </ul>
       </div>
     </div>
   );
